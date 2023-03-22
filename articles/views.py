652bf525599749bb99ea0063306a4f45adaee7d7
HY <9hiyoung@gmail.com>
2023-03-23 01:21:51 +0900

    django

---
+++ articles/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Article
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    article = Article(title=title, content=content)
+    article.save()
+
+    return render(request, 'articles/create.html')
